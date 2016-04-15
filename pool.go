4d48793e391312cc46fc049bbd85d7b5dd1e0ae7
Igor Mihalik <igor.mihalik@gmail.com>
2016-04-15 15:22:49 +0200

    init

---
+++ pool.go
@@ -0,0 +1,33 @@
+package pool
+
+import (
+	"runtime"
+	"sync"
+)
+
+type WorkerPool struct {
+	wg   sync.WaitGroup
+	once sync.Once
+	jobs chan func()
+}
+
+func (p *WorkerPool) Do(f func()) {
+	p.once.Do(func() {
+		p.jobs = make(chan func(), runtime.NumCPU()*2)
+		for i := 0; i < runtime.NumCPU(); i++ {
+			p.wg.Add(1)
+			go func() {
+				defer p.wg.Done()
+				for job := range p.jobs {
+					job()
+				}
+			}()
+		}
+	})
+	p.jobs <- f
+}
+
+func (p *WorkerPool) Done() {
+	close(p.jobs)
+	p.wg.Wait()
+}
